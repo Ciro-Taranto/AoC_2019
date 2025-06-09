9e0f1da1da2239452bb8c5309291c3f185e0b57e
Ciro-Taranto <ciro.taranto.ifp@gmail.com>
2025-06-09 09:18:38 +0200

    much uglier code just to support numba

--- d12.py
+++ d12.py
@@ -1,31 +1,27 @@
 from __future__ import annotations
 import itertools
 from dataclasses import dataclass
-from tqdm import tqdm
 import math
+import numba
+
+Position = Velocity = tuple[int, int, int]
 
 
-def astuple(position: Position) -> tuple[int, int, int]:
-    return position.x, position.y, position.z
+def astuple(position: Position) -> Position:
+    return position
 
 
-@dataclass
-class Position:
-    x: int
-    y: int
-    z: int
-
-    # Yes: type annotation is wrong, 3.10 does not have Self.
-    def __add__(self, other: Velocity) -> Position:
-        return Position(self.x + other.x, self.y + other.y, self.z + other.z)
-
-    def energy(self) -> int:
-        return sum(map(abs, astuple(self)))
+@numba.njit
+def add(a: tuple[int, int, int], b: tuple[int, int, int]) -> tuple[int, int, int]:
+    return a[0] + b[0], a[1] + b[1], a[2] + b[2]
 
 
-class Velocity(Position):
-    def invert(self) -> Velocity:
-        return Velocity(-self.x, -self.y, -self.z)
+def energy(position: Position) -> int:
+    return sum(abs(v) for v in position)
+
+
+def invert(position: Position) -> Position:
+    return tuple([-v for v in position])
 
 
 @dataclass
@@ -34,36 +30,42 @@ class State:
     velocity: Velocity
 
     def energy(self) -> int:
-        return self.position.energy() * self.velocity.energy()
+        return energy(self.position) * energy(self.velocity)
 
 
-def direction_vector(first: Position, second: Position) -> Velocity:
-    return Velocity(
-        *[unit_direction(p1, p2) for p1, p2 in zip(astuple(first), astuple(second))]
+@numba.njit
+def direction_vector(
+    first: tuple[int, int, int], second: tuple[int, int, int]
+) -> tuple[int, int, int]:
+    # ugly implement to help numba understand
+    return (
+        unit_direction(first[0], second[0]),
+        unit_direction(first[1], second[1]),
+        unit_direction(first[2], second[2]),
     )
 
 
+@numba.njit
 def unit_direction(p1: int, p2: int) -> int:
     if p1 == p2:
         return 0
-    return abs(p2 - p1) / (p2 - p1)
+    return abs(p2 - p1) // (p2 - p1)
 
 
 class System:
     def __init__(self, states: list[State]):
         self.states = states
-        self.visited = {direction: {} for direction in "xyz"}
         self.steps = 0
 
     def update_gravity(self):
         for first, second in itertools.combinations(self.states, 2):
             vector = direction_vector(first.position, second.position)
-            first.velocity += vector
-            second.velocity += vector.invert()
+            first.velocity = add(first.velocity, vector)
+            second.velocity = add(second.velocity, invert(vector))
 
     def update_positions(self):
         for state in self.states:
-            state.position += state.velocity
+            state.position = add(state.position, state.velocity)
 
     def update(self) -> None:
         self.update_gravity()
@@ -71,21 +73,22 @@ class System:
 
     def part_two(self) -> int:
         cycles = {}
+        visited = {i: {} for i in range(3)}
+
         for i in itertools.count():
-            for direction, visited in self.visited.items():
-                positions = tuple(
-                    [getattr(state.position, direction) for state in self.states]
-                )
-                velocities = tuple(
-                    [getattr(state.velocity, direction) for state in self.states]
-                )
-                if (positions, velocities) in visited and direction not in cycles:
-                    t = visited[positions, velocities]
+            for direction, visited_in_direction in visited.items():
+                positions = tuple([state.position[direction] for state in self.states])
+                velocities = tuple([state.velocity[direction] for state in self.states])
+                if (
+                    positions,
+                    velocities,
+                ) in visited_in_direction and direction not in cycles:
+                    t = visited_in_direction[positions, velocities]
                     p = i - t
                     cycles[direction] = (t, p)
                     print(f"Cycle for {direction}: period={p}, start={t}")
                 else:
-                    visited[(positions, velocities)] = i
+                    visited_in_direction[(positions, velocities)] = i
             self.update()
             if len(cycles) == 3:
                 break
@@ -103,12 +106,12 @@ def part_one(system: System) -> int:
 
 if __name__ == "__main__":
     positions = [
-        Position(x=5, y=13, z=-3),
-        Position(x=18, y=-7, z=13),
-        Position(x=16, y=3, z=4),
-        Position(x=0, y=8, z=8),
+        (5, 13, -3),
+        (18, -7, 13),
+        (16, 3, 4),
+        (0, 8, 8),
     ]
-    system = System([State(position, Velocity(0, 0, 0)) for position in positions])
+    system = System([State(position, (0, 0, 0)) for position in positions])
     print(part_one(system))
-    system = System([State(position, Velocity(0, 0, 0)) for position in positions])
+    system = System([State(position, (0, 0, 0)) for position in positions])
     print(system.part_two())
